91e710dc7fb2c6202cf70ffa6652d077b66fed18
diloretoignacio <diloretoignacio@gmail.com>
2023-06-08 16:07:02 -0300

    addElements
    Se permite agregar nuevos elementos

--- app/src/main/java/com/example/proconnect/HomeActivity.java
+++ app/src/main/java/com/example/proconnect/HomeActivity.java
@@ -1,22 +1,46 @@
 package com.example.proconnect;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private Button btn_CerrarSesion;
+    private FirebaseFirestore db;
     private TextView textViewEmail;
 
+    private Button btn_Agregarempleo;
+    private Button btn_CerrarSesion;
+
     private String email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +50,25 @@ public class HomeActivity extends AppCompatActivity {
         //Obtiene parametros recibidos del insert y lo pinta en el TextView
         email = getIntent().getStringExtra("email");
         textViewEmail = findViewById(R.id.emailTextView);
-        textViewEmail.setText("Email: " + email);
+        textViewEmail.setText("Usuario conectado: " + email);
 
-        //Obtiene boton de cerrar sesion
+        db = FirebaseFirestore.getInstance();
+
+        //Obtiene botones
         btn_CerrarSesion = findViewById(R.id.btn_CerrarSesion);
+        btn_Agregarempleo = findViewById(R.id.btn_agregarEmpleo);
 
-        //Asigna listener de click al boton de cerrar sesion
+        //Asigna listener a los botones
+        //Agregar Empleo
+        btn_Agregarempleo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, addActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Cerrar sesion
         btn_CerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +76,35 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        db.collection("empleos")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            mostrarDatos(task.getResult());
+                        } else {
+                            Toast.makeText(HomeActivity.this, "ERROR",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+
+
+
+
+
+    }
+
+
+    //Se obtienen los datos y se hace algo con ellos
+    private void mostrarDatos(QuerySnapshot datos){
+        for (QueryDocumentSnapshot document : datos) {
+            Object elemento = document.getData();
+        }
     }
 
     //Cierra sesion y muestra la pantalla de Auth
---
+++ app/src/main/java/com/example/proconnect/addActivity.java
@@ -0,0 +1,105 @@
+package com.example.proconnect;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class addActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private EditText editTextTitulo;
+    private EditText editTextDescripcion;
+    private EditText editTextTipo;
+    private EditText editTextUbicacion;
+
+    private Button btn_Agregar;
+    private Button btn_Volver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        db = FirebaseFirestore.getInstance();
+
+        //Obtiene EditText
+        editTextTitulo = findViewById(R.id.editTextTitulo);
+        editTextDescripcion = findViewById(R.id.editTextDescripcion);
+        editTextTipo = findViewById(R.id.editTextTipo);
+        editTextUbicacion = findViewById(R.id.editTextUbicacion);
+
+        //Obtiene botones
+        btn_Agregar = findViewById(R.id.btnAgregar);
+        btn_Volver = findViewById(R.id.btnVolver);
+
+        //Asigna listener a los botones
+        //Agregar empleo
+        btn_Agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String titulo = editTextTitulo.getText().toString();
+                String descripcion = editTextDescripcion.getText().toString();
+                String tipo = editTextTipo.getText().toString();
+                String ubicacion = editTextUbicacion.getText().toString();
+
+                Map<String, Object> empleo = new HashMap<>();
+                empleo.put("titulo", titulo);
+                empleo.put("descripcion", descripcion);
+                empleo.put("tipo", tipo);
+                empleo.put("ubicacion", ubicacion);
+                empleo.put("fecha", new Timestamp(new Date()));
+                empleo.put("imagen", "https://play-lh.googleusercontent.com/Gs6kFTfe9wy0kp3RvMMhCEejwohHaVUEaY9mda3aweBM9S6BLjLo7Nu4uTNNDN9gPfk=w240-h480-rw");
+                agregarEmpleo(empleo);
+                Toast.makeText(addActivity.this, "Empleo agregado correctamente", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(addActivity.this, AuthActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_Volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(addActivity.this, AuthActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //Agrega un empleo a la BD
+    private void agregarEmpleo(Map<String, Object> empleo){
+        db.collection("empleos").document(UUID.randomUUID().toString())
+                .set(empleo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+}
